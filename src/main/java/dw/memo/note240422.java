15cd0fed70b59bba0572ac945959379962045c02
pigking51 <145522570+pigking51@users.noreply.github.com>
2024-04-22 14:53:09 +0900

    240422중간

---
+++ src/main/java/dw/memo/note240422.java
@@ -0,0 +1,20 @@
+package dw.memo;
+
+public class note240422 {
+
+    // 4/22~23(월,화) : testdb의 내용을 JPA로 구현
+    //                 SQL로 다뤘던 연습문제들을 JPA로 구현
+    // 4/24(수) : 1) Service모듈에서 자바에서 구현하는 방법(제일중요!!!)
+    //           2) JPA에서 SQL을 구현하는 방법(=JPQL)
+    //           3) Repository에서 JPA메소드 명명법을 사용하는 방법
+    //              → 구글에서 JPA 쿼리메소드 치면 관련내용 나옴
+    //           → 2,3번은 조금 난해하기에 1번위주로 강의하실 예정이라고 함
+    //           → 1,2,3번순으로 우선순위(1번 가장중요, 2번 현실적 목적)
+    // 4/25(목) : 스프링 시큐리티 - 로그인 / 로그아웃(ID, PW)
+    
+    // 기존 5개 패키지 말고 DTO, Utility, DAO패키지를 사용하는 예제코드도 있음
+    // Service안에 impl 파일 존재하는 경우도 있는데
+    // 그 안에 XXXServiceimpl.java가 안에 있음
+    // → 이 경우에는 상위폴더인 Service패키지에 XXXService.java라는 인터페이스가 존재하고
+    //   해당 인터페이스에 대한 구현체가 XXXServiceimpl.java라는 것임 
+}
